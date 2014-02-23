1c7fd3ac2d3d0f96f8849242b56bb59b693984f6
cuspace <cuspace@gmail.com>
2014-02-24 02:07:11 +0900

    setting the acceleration, velocity
    - it’s fun with changing the number that is used to update p.vx, p.vy
    and p.x, p.y.

--- js/main.js
+++ js/main.js
@@ -2,8 +2,12 @@ var g;
 var mouseX = 0, mouseY = 0;
 
 var p = {
-	x: 200,
-	y: 200
+  x: 200,
+  y: 200,
+  vx: 0,
+  vy: 0,
+  ax: 0,
+  ay: 0
 };
 
 var init = function() {
@@ -14,15 +18,21 @@ var init = function() {
 };
 
 var update = function() {
-  p.x = mouseX //- p.x;
-  p.y = mouseY //- p.y;
+  p.ax = mouseX - p.x;
+  p.ay = mouseY - p.y;
+  p.vx += p.ax * 0.1;
+  p.vy += p.ay * 0.1;
+  p.x += p.vx * 0.1;
+  p.y += p.vy * 0.1;
+  p.vx *= 0.8;
+  p.vy *= 0.8;
 };
 
 var draw = function() {
 	g.clearRect(0, 0, g.canvas.width, g.canvas.height);
-	g.fillStyle = 'rgba(0, 0, 0, 0.5)';
+	g.fillStyle = 'rgba(0, 0, 0, 0.2)';
 	g.beginPath();
-	var radius = 10;
+	var radius = 5;
 	g.arc(p.x, p.y, radius, 0, Math.PI * 2);
 	g.fill();
 };
